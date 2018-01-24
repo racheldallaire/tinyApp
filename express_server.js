063776880b459a460ab479dc8ee9f06d2764d652
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-01-24 21:13:06 +0000

    Reordered code on server page to improve readability

--- express_server.js
+++ express_server.js
@@ -6,7 +6,12 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+
 var PORT = process.env.PORT || 8080; // default port 8080
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -22,20 +27,17 @@ function generateRandomString() {
 return random;
 }
 
-app.post("/urls", (req, res) => {
-
-urlDatabase[generateRandomString()] = req.body.longURL;
-res.redirect("/urls");
-
+app.get("/", (req, res) => {
+  res.end("Hello!");
 });
-
-app.set("view engine", "ejs");
-
-app.use(express.static('public'));
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -54,13 +56,11 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
-});
+app.post("/urls", (req, res) => {
 
+urlDatabase[generateRandomString()] = req.body.longURL;
+res.redirect("/urls");
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
 app.post("/urls/:id", (req, res) => {
@@ -84,5 +84,5 @@ app.post("/login", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Listening on port ${PORT}!`);
 });
