5c75cec6b2b4352906ab308410424f7e4d459afd
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-01-28 18:11:00 +0000

    Now using encrypted cookies

--- express_server.js
+++ express_server.js
@@ -1,55 +1,27 @@
 //Required frameworks and packages
 const express = require("express");
 const cookieSession = require('cookie-session');
-const cookieParser = require('cookie-parser'); //DELETE THIS LATER
 const bodyParser = require("body-parser");
 const bcrypt = require('bcrypt');
 
-var app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
-
+const app = express();
 app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+//Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['Go buy a chopper and have a doctor on speed dial I guess - M.A.A.d city'],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
+
 
 //Setting port to default
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-//Object containing all URL info
-var urlDatabase = {
-  "b2xVn2": {
-    shortURL: "b2xVn2",
-    longURL: "http://www.lighthouselabs.ca",
-    userID: "LiamTheChampion"
-    // views: 0
-    // uniqueVisits: 0
-    // createdAt: new Date();
-  },
-
-  "9sm5xK": {
-    shortURL: "9sm5xK",
-    longURL: "http://www.google.com",
-    userID: "rachie.dxo"
-    // views: 0
-    // uniqueVisits: 0
-    // createdAt: newDate(),
-  }
-};
-
-//Object containing all user information
-const users = {
-  "LiamTheChampion": {
-    id: "LiamTheChampion",
-    email: "liam@example.com",
-    password: "baseball-123"
-  },
-  "rachie.dxo": {
-    id: "rachie.dxo",
-    email: "rachie@example.com",
-    password: "123"
-  },
-};
+//Objects containing all URL info & user information
+const urlDatabase = {};
+const users = {};
 
 //Function to generate 6 digit alphanumeric string, used to generate short URLs
 function generateRandomString() {
@@ -82,7 +54,7 @@ app.get("/", (req, res) => {
 
 //Main URLs page
 app.get("/urls", (req, res) => {
-  let user_id = req.cookies["user_id"];
+  let user_id = req.session.user_id;
   let display = urlsForUser(user_id);
   if (!user_id) {
     res.redirect('/login');
@@ -99,7 +71,7 @@ app.get("/urls.json", (req, res) => {
 
 //Create new URL page (with function only allowing access to logged in users)
 app.get("/urls/new", (req, res) => {
-  let user_id = req.cookies["user_id"];
+  let user_id = req.session.user_id;
   function validateUser () {
     let validate = true;
     if (!user_id) {
@@ -117,7 +89,7 @@ app.get("/urls/new", (req, res) => {
 
 //Edit short URL page
 app.get("/urls/:id", (req, res) => {
-  let user_id = req.cookies["user_id"];
+  let user_id = req.session.user_id;
   let templateVars = { shortURL: req.params.id, fullURL: urlDatabase[req.params.id].longURL, user: users[user_id] };
   if(urlDatabase[req.params.id].userID !== user_id) {
     res.status(400).send('You cannot edit a URL you did not create.');
@@ -137,7 +109,7 @@ app.get("/u/:shortURL", (req, res) => {
     res.status(401).send('Please enter a valid URL.');
     return null;
   }
-  let user_id = req.cookies["user_id"];
+  let user_id = req.session.user_id;
   let templateVars = { user: users[user_id] };
   res.render(templateVars);
 });
@@ -149,7 +121,7 @@ app.get("/register", (req, res) => {
 
 //Login page
 app.get("/login", (req, res) => {
-  let user_id = req.cookies["user_id"];
+  let user_id = req.session.user_id;
   let display = urlsForUser(user_id);
   if (user_id) {
     res.redirect('/urls');
@@ -165,7 +137,7 @@ app.post("/urls", (req, res) => {
   urlDatabase[shortURL] = {
     shortURL : shortURL,
     longURL: req.body.longURL,
-    userID: req.cookies["user_id"]
+    userID: req.session.user_id
   } ;
   res.redirect("/urls/"/* + shortURL */);
   //N.B. I was asked to redirect to the edit page after creating a new URL, but this did not seem logical to me so I am redirecting back to the main urls page instead. I left the code that I was asked to produce in comments to show that I know how to do it, but would rather not for the sake of a better user experience.
@@ -179,7 +151,7 @@ app.post("/urls/:id", (req, res) => {
 
 app.post("/urls/:id/delete", (req, res) => {
   let shortURL = req.params.id;
-  let user_id = req.cookies["user_id"];
+  let user_id = req.session.user_id;
   if(urlDatabase[shortURL].userID !== user_id) {
     res.status(400).send('You cannot delete a URL you did not create.');
     return null;
@@ -192,20 +164,20 @@ app.post("/login", (req, res) => {
   let emaily = req.body.email;
   let passwordy = req.body.password;
   let flag = true;
-for(let userid in users) {
-  if(users[userid].email === emaily && bcrypt.compareSync(passwordy, users[userid].password)) {
-    res.cookie('user_id', users[userid].id);
-    res.redirect('/urls');
-    flag = false;
+  for(let userid in users) {
+    if(users[userid].email === emaily && bcrypt.compareSync(passwordy, users[userid].password)) {
+      req.session.user_id = users[userid].id;
+      res.redirect('/urls');
+      flag = false;
+    }
   }
-}
   if (flag) {
     res.status(403).send("Incorrect login details.");
   }
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect('/urls');
 });
 
@@ -229,7 +201,7 @@ app.post("/register", (req, res) => {
     email: emaily,
     password: hashedPassword
   };
-  res.cookie('user_id', newid);
+  req.session.user_id = newid;
   res.redirect('/urls');
   } else {
     res.status(400).send(errorMessage);
